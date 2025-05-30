5daa92d1731ab4e9193bbe4a3a0a2ae538abca37
hen002 <henrywidodo6@gmail.com>
2025-05-30 08:33:18 +0700

    Styling App Router

---
+++ layout/index.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+
+export default function MainLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div>
+
+            <div
+                className="flex flex-col min-h-screen"
+            >
+                <header className="bg-blue-600 text-white">
+                    <div className="container mx-auto flex justify-between items-center">
+                        <h1 className="text-xl font-bold">Next.js App</h1>
+                        <ul className="flex space-x-4">
+                            <li><Link href="/">Home</Link></li>
+                            <li><Link href='/about'>About</Link></li>
+                        </ul>
+                    </div>
+                </header>
+
+                <main className="flex-1 container mx-auto p-4">
+                    {children}
+                </main>
+
+                <footer className="bg-gray-800 text-white p-4 text-center">
+                    <p>&copy; {new Date().getFullYear()} Henry Website</p>
+                </footer>
+            </div>
+        </div>
+    )
+
+}

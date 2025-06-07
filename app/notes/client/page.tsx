eaf0ff833cce549c56de81d2556b55d7ae5ec420
hen002 <henrywidodo6@gmail.com>
2025-06-07 09:52:56 +0700

    Data Fetching App Router

---
+++ app/notes/client/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+
+export default function Notes() {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('https://service.pace11.my.id/api/notes')
+            .then((res) => res.json())
+            .then((data) => setData(data?.data || []))
+            .finally(() => setLoading(false))
+
+    }, [])
+
+    if (loading) return <div>Loading... </div>
+
+    return (
+        <ul>
+            {data?.map((el: { id: number; title: string }) =>
+            (
+                <li key={el.id}>{el.title}</li>
+            )) || []}
+        </ul>
+    )
+}
---
+++ app/notes/server/page.tsx
@@ -0,0 +1,18 @@
+async function getNotes() {
+    const notes = await fetch('https://service.pace11.my.id/api/notes')
+        .then((res) => res.json())
+    return notes
+}
+
+export default async function Notes() {
+    const notes = await getNotes()
+
+    return (
+        <ul>
+            {notes.data.map((el: { id: number; title: string }) =>
+            (
+                <li key={el.id}>{el.title}</li>
+            ))}
+        </ul>
+    )
+}
